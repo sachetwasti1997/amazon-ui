1847006bb20a1f89f4383db37e009fc41ca274f2
sachetwasti1997 <sachetwasti61@gmail..com>
2024-03-02 18:49:28 +0530

    Bearer token addition

--- src/container/HomePage.jsx
+++ src/container/HomePage.jsx
@@ -1,9 +1,29 @@
 import { useState } from "react";
-import { Navigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import SignUpForm from "../container/SignUpForm";
+import axios from "axios";
+import { API_BASE_PATH } from "../Constants";
 
 const HomePage = () => {
   const [isLogged, setIsUserLogged] = useState(false);
+  const [userData, setUserData] = useState({});
+
+  const getUser = (token) => {
+    console.log(token);
+    axios
+      .get(API_BASE_PATH + "/user/profile", {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      })
+      .then((res) => {
+        setUserData(res.data);
+      })
+      .catch((err) => {
+        localStorage.removeItem("token");
+        setIsUserLogged(false);
+      });
+  };
 
   useState(() => {
     const bearerToken = localStorage.getItem("token");
@@ -12,10 +32,15 @@ const HomePage = () => {
       setIsUserLogged(false);
     } else {
       setIsUserLogged(true);
+      getUser(bearerToken);
     }
   }, []);
 
-  return <div>{isLogged ? "" : <Navigate to={"/signup"} />}</div>;
+  return (
+    <div>
+      {isLogged ? JSON.stringify(userData) : <Navigate to={"/signup"} />}
+    </div>
+  );
 };
 
 export default HomePage;
